a0823cfb1a89b68191dcdc96f59d70e2889162ba
bressawsk <thiago@freecode.com.br>
2021-04-25 15:58:25 -0300

    Add files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eeb51636f23d12df970ca64d1d387a68'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b7fec349-c529-4d8b-b0e7-74dc32b48e3b'
+};
